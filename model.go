d039f4faed8e937898668680c0efdaa3e583c893
Syraifang <email_akun_github_mu@email.com>
2026-08-23 19:05:04 +0700

    inisialisasi tugas 2  dan membuat model student

---
+++ model.go
@@ -0,0 +1,61 @@
+package main
+
+// Student adalah entitas utama di dalam memori
+type Student struct {
+	ID       int     `json:"id"`
+	NIM      string  `json:"nim"`
+	Name     string  `json:"name"`
+	Grade    float64 `json:"grade"`
+	IsActive bool    `json:"is_active"`
+}
+
+// CreateStudentRequest untuk metode POST (semua wajib)
+type CreateStudentRequest struct {
+	NIM      string  `json:"nim"`
+	Name     string  `json:"name"`
+	Grade    float64 `json:"grade"`
+	IsActive bool    `json:"is_active"`
+}
+
+// ReplaceStudentRequest untuk metode PUT (ganti seluruhnya, semua wajib)
+type ReplaceStudentRequest struct {
+	NIM      string  `json:"nim"`
+	Name     string  `json:"name"`
+	Grade    float64 `json:"grade"`
+	IsActive bool    `json:"is_active"`
+}
+
+// PatchStudentRequest untuk metode PATCH (ubah sebagian, pakai pointer)
+type PatchStudentRequest struct {
+	NIM      *string  `json:"nim,omitempty"`
+	Name     *string  `json:"name,omitempty"`
+	Grade    *float64 `json:"grade,omitempty"`
+	IsActive *bool    `json:"is_active,omitempty"`
+}
+
+// Amplop baku untuk semua respons API
+type WebResponse struct {
+	Success bool   `json:"success"`
+	Message string `json:"message"`
+	Data    any    `json:"data,omitempty"`
+	Meta    *Meta  `json:"meta,omitempty"`
+	Errors  any    `json:"errors,omitempty"`
+}
+
+// Meta untuk informasi paginasi
+type Meta struct {
+	Page       int `json:"page"`
+	Limit      int `json:"limit"`
+	Total      int `json:"total"`
+	TotalPages int `json:"total_pages"`
+}
+
+// ListQuery untuk menangkap parameter pencarian, pengurutan, dll
+type ListQuery struct {
+	Page     int
+	Limit    int
+	Search   string
+	Sort     string
+	Order    string
+	IsActive *bool
+}
